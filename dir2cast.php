64480b6f205c5903744e1c5437d3c18f6c0d496f
Oskar Joelson <oskar@joelson.org>
2019-11-21 12:24:35 +0100

    Set time to noon if using id3 year

--- dir2cast.php
+++ dir2cast.php
@@ -425,7 +425,7 @@ class RSS_Item extends GetterSetter {
         $item_elements = array(
             'title' => $this->getTitle(),
             'link' => $this->getLink(),
-            'pubDate' => $this->getID3Year() && strlen($this->getID3Year()) >= 8 ? date('r', strtotime($this->getID3Year())) : $this->getPubDate()
+            'pubDate' => $this->getID3Year() && strlen($this->getID3Year()) == strlen('YYYY-MM-DD') ? date('r', strtotime($this->getID3Year() . "12:00:00")) : $this->getPubDate()
         );
 
         if(DESCRIPTION_SOURCE == 'file')
